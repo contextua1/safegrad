368a201be1923008aaa2a413d73249f929be57ec
contextua1 <suheda.kozan@boun.edu.tr>
2025-05-18 11:53:22 +0300

    implemented functionality and tests for gradient accumulation logic

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,22 @@ fn main() {
     println!("SafeGrad: A Rust implementation of micrograd");
 
     // Toy example: build a small graph
-    let a = Value::new(2.0, Some("a".to_string()));
-    let b = Value::new(-3.0, Some("b".to_string()));
-    let c = Value::add(
-        &a, 
-        &Value::mul(&b, &a, Some("b*a".to_string())), 
-        Some("c = a+(b*a)".to_string())
-    );
-    let d = Value::mul(&c, &b, Some("d = c*b".to_string()));
-
-    println!("d = b*a + b*(b * a) = {}", Value::data(&d));
+    let a = Value::new(-2.0, Some("a".to_string()));
+    let b = Value::new(3.0, Some("b".to_string()));
+    let e = Value::add(&a, &b, Some("e =a+b".to_string()));
+    let d = Value::mul(&a, &b, Some("d = a*b".to_string()));
+    let f = Value::mul(&d, &e, Some("f = d*e".to_string()));
+    println!("f = d*e = {}", Value::data(&f));
 
     // Backward pass
-    Value::backward(&d);
+    Value::backward(&f);
     println!("Gradient of a: {}", Value::grad(&a));
     println!("Gradient of b: {}", Value::grad(&b));
-    println!("Gradient of c: {}", Value::grad(&c));
-    println!("Gradient of d: {}", Value::grad(&c));
-
+    println!("Gradient of e: {}", Value::grad(&e));
+    println!("Gradient of d: {}", Value::grad(&d));
+    println!("Gradient of f: {}", Value::grad(&f));
     // Export graph to DOT format
-    let dot = Value::export_dot(&d);
+    let dot = Value::export_dot(&f);
     std::fs::write("graph.dot", &dot).expect("Failed to write graph.dot");
     println!("Graph exported to graph.dot. You can render it with: dot -Tpng graph.dot -o graph.png");
 }
